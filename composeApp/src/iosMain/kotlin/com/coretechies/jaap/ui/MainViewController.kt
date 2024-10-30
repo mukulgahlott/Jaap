b74e900666c7b157d49256c0786b3e3b3ded5a74
JunedM <junedmughal27@gmail.com>
2024-10-30 16:40:50 +0530

    M1-changes

--- composeApp/src/iosMain/kotlin/com/coretechies/jaap/ui/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/coretechies/jaap/ui/MainViewController.kt
@@ -1,5 +1,18 @@
 package com.coretechies.jaap.ui
 
+import HomeScreen
+import MenuScreen
 import androidx.compose.ui.window.ComposeUIViewController
+import com.coretechies.jaap.screens.ListScreen
 
-fun MainViewController() = ComposeUIViewController {  }
+fun HomeScreenViewController() = ComposeUIViewController {
+//        HomeScreen()
+    }
+
+    fun ListScreenViewController() = ComposeUIViewController {
+//        ListScreen()
+    }
+
+    fun MenuScreenViewController() = ComposeUIViewController {
+//        MenuScreen()
+    }
